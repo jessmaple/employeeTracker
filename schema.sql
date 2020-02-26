cd75b70763256fb952643e5d9e7056f82da02b8a
Jessica Maple <maple@jessicas-mbp-4.attlocal.net>
2020-02-26 07:18:39 -0500

    wip

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE database employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE employee(
+    id INT not null auto_increment,
+    first_name varchar (30) not null, 
+    last_name varchar (30) not null, 
+    role_id int not null, 
+    manager_id int, 
+    PRIMARY KEY(id)
+) ;
+
+CREATE TABLE role (
+    id INT not null auto_increment,
+    title VARCHAR (30) not null,
+    salary DECIMAL not null,
+    department_id INT, 
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE deparment (
+   id INT not null auto_increment,
+   name VARCHAR(30),
+); 
