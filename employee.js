a23b47a00eae5ad9905ad04e8d8044eb7b445c54
Jessica Maple <maple@jessicas-mbp-4.attlocal.net>
2020-04-25 15:53:13 -0400

    updates

--- employee.js
+++ employee.js
@@ -1,15 +1,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-require("console.table")
+require("console.table");
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
-  database: "employee_DB"
+  database: "employee_DB",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.log(err);
   }
@@ -19,126 +19,146 @@ connection.connect(function(err) {
 
 // function
 function menu() {
-  inquirer.prompt([
-    {
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "View all employees",
-        "View All employees by deparment",
-        "View all employees by manager",
-        "add employee",
-        "remove employee",
-        "Update employee role",
-        "update employee manager",
-        "view all roles",
-        "add role",
-        "remove role"
-      ], 
-      name: "menuList"
-    }
-  ]).then(function(input){
-      console.log(input.menuList)
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "View All employees by deparment",
+          "View all employees by manager",
+          "add employee",
+          "remove employee",
+          "Update employee role",
+          "update employee manager",
+          "view all roles",
+          "add role",
+          "remove role",
+        ],
+        name: "menuList",
+      },
+    ])
+    .then(function (input) {
+      console.log(input.menuList);
 
-      switch(input.menuList){
-        case "View all employees": 
-        viewAllEmployees()
-        break
+      switch (input.menuList) {
+        case "View all employees":
+          viewAllEmployees();
+          break;
         case "View All employees by deparment":
-          byDepartment()
-        break
+          byDepartment();
+          break;
         case "View all employees by manager":
-          byManager()
-        break
+          byManager();
+          break;
         case "add employee":
-          addEmployee()
-        break
+          addEmployee();
+          break;
         case "remove employee":
-          removeEmployee()
-        break
+          removeEmployee();
+          break;
         case "Update employee role":
-          updateRole()
-        break
+          updateRole();
+          break;
         case "update employee manager":
-          updateManager()
-        break
+          updateManager();
+          break;
         case "view all roles":
-          viewAllRoles()
-        break
+          viewAllRoles();
+          break;
         case "add role":
-          addRole()
-        break
+          addRole();
+          break;
         case "remove role":
-          removeRole()
+          removeRole();
       }
-    
-  })
+    });
 }
-function viewAllEmployees(){
-connection.query(`
+function viewAllEmployees() {
+  connection.query(
+    `
 SELECT A.id, A.first_name, A.last_name, C.title, concat_ws(' ',nullif(B.first_name,' ' ) , nullif(B.last_name, ' ' ))  manager  
 FROM employee A 
 left join employee B   on A.manager_id=B.id 
 left join role C on A.role_id = C.id
-`, function(err, data){
-console.table(data)
-menu()
-
-})
+`,
+    function (err, data) {
+      console.table(data);
+      menu();
+    }
+  );
 }
 
-function  byDepartment(){
-connection.query(`
+function byDepartment() {
+  connection.query(
+    `
 SELECT A.id, A.first_name, A.last_name, C.title,D.name department, concat_ws(' ',nullif(B.first_name,' ' ) , nullif(B.last_name, ' ' ))  manager  
 FROM employee A 
 left join employee B   on A.manager_id=B.id 
 left join role C on A.role_id = C.id
 left join department D on C.department_id=D.id order by department
-`, function(err, data){
-  console.table(data)
-  menu()
-})
+`,
+    function (err, data) {
+      console.table(data);
+      menu();
+    }
+  );
 }
 
-function  byManager(){
-  connection.query(`
+function byManager() {
+  connection.query(
+    `
   SELECT A.id, A.first_name, A.last_name, C.title, E.manager name, concat_ws(' ' ,,nullif(B.first_name,' ' ) , nullif(B.last_name, ' ' )) manager 
   FROM employee A
   left join employee B   on A.manager_id=B.id 
   left join role C on A.role_id = C.id
   left join manager E on a.role_id = C.id order by manager 
-  `, function(err, data){
-    console.table(data)
-    menu()
-  `)
-
+  `,
+    function (err, data) {
+      console.table(data);
+      menu();
+    }
+  );
 }
 
-function   addEmployee(){
-
+function lookup(tableName, columnName){
+  return new Promise ((resolve, reject)=>{
+    connection.query(`select ${columnName}`)
+  })
 }
 
-function  removeEmployee(){
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your first name?",
+      name: "firstName",
+    },
+    {
+      type: "input",
+      message: "What is your last name?",
+      name: "lastName",
+    },
+    {
+      type:"list",
+      message:"Choose your role",
+      name:"role",
 
+    }
+  ]);
+
+  //connection.query(`Insert Into employee ()`)
 }
 
-function updateRole(){
+function removeEmployee() {}
 
-}
+function updateRole() {}
 
-function  updateManager(){
+function updateManager() {}
 
-}
+function viewAllRoles() {}
 
-function viewAllRoles(){
+function addRole() {}
 
-}
-
-function addRole(){
-
-}
-
-function removeRole() {
-
-}
-
+function removeRole() {}
